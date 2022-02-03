cae5c77f22e2063bf41605a06840a94281130587
rayhad <hadidrayann93@gmail.com>
2022-02-03 11:58:41 +0100

    react-piscine2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">React-Piscine !</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Accueil</Nav.Link>
+          <Nav.Link href="#CarnetdeNote">Carnet de note</Nav.Link>
+          <Nav.Link href="#CreationNote">Création d'une note</Nav.Link>
+          <Nav.Link href="#PrevNote">Prévisualition d'une note</Nav.Link>
+          <Nav.Link href="#ModifNote">Modification d'une note</Nav.Link>
+          <Nav.Link href="#GlobalConfig">Configuration globale</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 }
-
-export default App;
